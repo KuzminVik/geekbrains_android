2c31b8cb5d50aa3fc6036031e23f32116779ffab
KuzminVik <viksimurg@gmail.com>
2020-11-22 21:31:25 +0300

    Седьмой урок, темы, стили и материальный дизайн

--- app/src/main/java/ru/geekbrains/myweather/Constants.java
+++ app/src/main/java/ru/geekbrains/myweather/Constants.java
@@ -5,6 +5,7 @@ public interface Constants {
     String KEY = "KEY";
     String KEY_1 = "KEY_1";
     String KEY_2 = "KEY_2";
+    boolean DEBUG = true;
 
 
 }
--- app/src/main/java/ru/geekbrains/myweather/MainActivity.java
+++ app/src/main/java/ru/geekbrains/myweather/MainActivity.java
@@ -1,32 +1,53 @@
 package ru.geekbrains.myweather;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 
-public class MainActivity extends AppCompatActivity implements Constants{
+public class MainActivity extends BaseActivity implements Constants{
+    private static final int SETTING_CODE = 88;
     private static final String TAG = "MyLog";
     private final String site = "https://www.calend.ru/events/";
     static Packet currentPacket;
+    private Toolbar toolbar;
 
     @SuppressLint({"SetTextI18n", "StringFormatMatches", "StringFormatInvalid"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Здесь вылетит Snackbar
+                Snackbar.make(view, "Вы нажали на плавающую кнопку", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
 
         //Получаем пакет от активити с выбором городов и передаем его в бандле в фрагмент
         if (savedInstanceState == null){
@@ -38,7 +59,6 @@ public class MainActivity extends AppCompatActivity implements Constants{
             bundle.putParcelable(KEY, currentPacket);
             f.setArguments(bundle);
             ft.replace((R.id.weather_framelayout), f).commit();
-
         }
 
         WeekDaySource sourceData = new WeekDaySource(getResources());
@@ -66,52 +86,52 @@ public class MainActivity extends AppCompatActivity implements Constants{
     @Override
     protected void onStart() {
         super.onStart();
-        Log.d(TAG, "Активити: onStart()");
+        if(DEBUG){
+            Log.v(TAG, "Активити: onStart()");
+        }
     }
 
     @Override
     protected void onRestoreInstanceState(Bundle saveInstanceState){
         super.onRestoreInstanceState(saveInstanceState);
         currentPacket = saveInstanceState.getParcelable(TEXT);
-
-        Log.d(TAG, "Активити: Повторный запуск! - onRestoreInstanceState()");
+        if(DEBUG){
+            Log.v(TAG, "Активити: Повторный запуск! - onRestoreInstanceState()");
+        }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.d(TAG, "Активити: onResume()");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.d(TAG, "Активити: onPause()");
     }
 
     @Override
     protected void onSaveInstanceState(@NonNull Bundle saveInstanceState){
         super.onSaveInstanceState(saveInstanceState);
         saveInstanceState.putParcelable(TEXT, currentPacket);
-        Log.d(TAG, "Активити: onSaveInstanceState()");
+        if(DEBUG){
+            Log.v(TAG, "Активити: onSaveInstanceState()");
+        }
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.d(TAG, "Активити: onStop()");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        Log.d(TAG, "Активити: onRestart()");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.d(TAG, "Активити: onDestroy()");
     }
 
     private void initRecyclerView(WeekDaySource sourceData){
@@ -123,5 +143,47 @@ public class MainActivity extends AppCompatActivity implements Constants{
         // Установим адаптер
         WeatherListAdapter adapter = new WeatherListAdapter(sourceData);
         recyclerView.setAdapter(adapter);
+    }
+
+    //Метод recreate() пересоздаст активити, тем самым применит новую тему.
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == SETTING_CODE){
+            recreate();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // разместить меню в action bar
+        // если он присутствует.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Управление касаниями на action bar.
+        int id = item.getItemId();
+        //noinspection SimplifiableIfStatement
+        switch (id){
+            case R.id.action_home:
+                Snackbar.make(toolbar, "Вы выбрали пункт меню Главная", Snackbar.LENGTH_LONG)
+                        .setAction("Кнопка", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Toast.makeText(MainActivity.this.getApplicationContext(), "Кнопка в Snackbar нажата", Toast.LENGTH_LONG).show();
+                            }
+                        }).show();
+                return true;
+            case R.id.action_settings:
+                Snackbar.make(toolbar, "Вы выбрали пункт меню Настройки", Snackbar.LENGTH_LONG).show();
+                return true;
+            case R.id.action_devs:
+                Snackbar.make(toolbar, "Вы выбрали пункт меню О разработчиках", Snackbar.LENGTH_LONG).show();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
